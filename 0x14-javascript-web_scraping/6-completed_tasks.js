e815a908736aa9c8fd0dbabc1ecfb77ac0d1434a
samuel mwika <36988997+samuelmwikacs@users.noreply.github.com>
2022-11-16 04:11:04 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
+const url = process.argv[2];
+request(url, function (error, response, body) {
   if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+    const taskCount = {};
+    const allTasks = JSON.parse(body);
+    for (const taskId in allTasks) {
+      const task = allTasks[taskId];
+      if (task.completed && taskCount[task.userId] === undefined) taskCount[task.userId] = 1;
+      else if (task.completed) taskCount[task.userId] += 1;
+    }
+    console.log(taskCount);
   }
 });
